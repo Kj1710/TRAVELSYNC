0949bb850071ad7dcebdaaeaf3fe0f6afd02a828
Kaushal Jha <you@example.com>
2024-01-21 19:59:17 +0530

    Code Updated

--- src/screens/Animation.js
+++ src/screens/Animation.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Image, StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
 
-const Animation = () => {
+const Animation = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Home');
+    }, 2000);
+  }, []);
+
   return (
-    <View>
-      <Text>Animation</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <StatusBar hidden />
+      <Image
+        style={{height: 250, width: '100%', resizeMode: 'contain'}}
+        source={{
+          uri: 'https://imgs.search.brave.com/TrVP65xB6EBuztA8xchQP9AoyNRJTjSUUu06RivgjU4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzg0LzM4LzY3/LzM2MF9GXzI4NDM4/Njc0NF85dnRPdHhz/STVqOHZKM1VEeURN/R3djcHBSOVA5RFlL/Qy5qcGc',
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Animation
+export default Animation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
