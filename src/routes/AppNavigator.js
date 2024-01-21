b17a77c45c9fea376c1f4f7a2e4b0dd6b4ddcf9e
Kaushal Jha <you@example.com>
2024-01-21 16:03:09 +0530

    Updated code

---
+++ src/routes/AppNavigator.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+  </Stack.Navigator>
+  )
+}
+
+export default AppNavigator
+
+const styles = StyleSheet.create({})
