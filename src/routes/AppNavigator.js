bbf79841253513b29be258c64381b8e9e8e4bdc3
Kaushal Jha <you@example.com>
2024-01-21 16:36:37 +0530

    file structure and Navigation done

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -1,17 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import Animation from '../screens/Animation';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator>
-    <Stack.Screen name="Home" component={HomeScreen} />
-  </Stack.Navigator>
-  )
-}
+    <Stack.Navigator
+      initialRouteName="Animation"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Animation" component={Animation} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
 
-export default AppNavigator
+export default AppNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
