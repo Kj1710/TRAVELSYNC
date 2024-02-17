9fe0e0187e86db6242ee0d89c8c6e11622835fc6
Kaushal Jha <you@example.com>
2024-02-17 09:06:29 +0530

    Logic Working Fine

--- src/screens/Destination.js
+++ src/screens/Destination.js
@@ -1,10 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {apiKey} from '../utils/apikey';
+import { getDistance } from 'geolib';
 
-const Destination = ({navigation}) => {
+const Destination = ({navigation, route}) => {
+  const userLocation=route?.params?.userLocation
+  const {latitude,longitude}=userLocation
+  console.log(latitude,longitude,'from origin');
+
+const handlePlaceClick=(data,details=null)=>{
+const {geometry}=details
+if (geometry&&geometry.location) {
+  const {lat:destLat,lng:destLong}=geometry.location
+  const distance=getDistance({latitude:latitude,longitude:longitude},{
+    latitude:destLat,longitude:destLong
+  })
+  const fromMetersToKms=distance/1000
+  console.log(fromMetersToKms,'kms');
+  if (fromMetersToKms<=50) {
+    navigation.navigate('Home', {details,fromMetersToKms});
+  }else{
+    Alert.alert("The destination is too far away.")
+  }
+} 
+
+}
+
+
+
   return (
     <View style={{flex: 1, padding: 20, gap: 20}}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
@@ -12,7 +37,7 @@ const Destination = ({navigation}) => {
           name="chevron-back"
           size={25}
           color="black"
-          onPress={() => navigation.goBack()}
+          onPresS={() => navigation.goBack()}
         />
         <Text
           style={{color: 'black', fontSize: 20, textAlign: 'center', flex: 1}}>
@@ -21,9 +46,10 @@ const Destination = ({navigation}) => {
       </View>
       <GooglePlacesAutocomplete
         placeholder="Search"
-        onPress={(data, details = null) => {
-          navigation.navigate('Home', {details});
-        }}
+        onPress={handlePlaceClick}
+        // onPress={(data, details = null) => {
+        //   navigation.navigate('Home', {details});
+        // }}
         query={{
           key: apiKey,
           language: 'en',
