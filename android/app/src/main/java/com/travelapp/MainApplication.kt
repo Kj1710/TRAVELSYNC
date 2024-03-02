b04ecea505850de3c7283e5b5e1efb3e18ca9cb8
Kaushal Jha <you@example.com>
2024-03-02 21:35:18 +0530

    updated

--- android/app/src/main/java/com/travelapp/MainApplication.kt
+++ android/app/src/main/java/com/travelapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rapido
+package com.travelapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/travelappTests/travelappTests.m
+++ ios/travelappTests/travelappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rapidoTests : XCTestCase
+@interface travelappTests : XCTestCase
 
 @end
 
-@implementation rapidoTests
+@implementation travelappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
